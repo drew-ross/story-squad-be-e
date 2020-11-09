c6cbe746a02e4aef1407be5e65a3df7c5eb0c6a0
christopher harwell <chrisharwell72@gmail.com>
2020-11-09 16:41:23 -0500

    Adds scaffolding for Ghost_Users table migration

--- data/migrations/20201104190917_gamification-bots.js
+++ data/migrations/20201104190917_gamification-bots.js
@@ -1,18 +1,11 @@
 
 exports.up = function (knex) {
-    return knex.schema.createTable('Player_Count', (t) => {
-        t.increments('ID');
-        t.integer('ChildID')
-            .notNullable()
-            .unsigned()
-            .references('Children.ID')
-            .onUpdate('CASCADE')
-            .onDelete('RESTRICT')
+    return knex.schema.createTable('Ghost_Users', (t) => {
         
     });
 };
 
 exports.down = function (knex) {
     return knex.schema
-        .dropTableIfExists('Player_Count')
+        .dropTableIfExists('Ghost_Users')
 };
