e2014159cb5a28bed3475d68cbd7a6da85a9a541
christopher harwell <chrisharwell72@gmail.com>
2020-11-10 17:20:40 -0500

    Adds schema to Ghost_Users Migration

--- data/migrations/20201104190917_gamification-bots.js
+++ data/migrations/20201104190917_gamification-bots.js
@@ -1,11 +1,29 @@
 
 exports.up = function (knex) {
     return knex.schema.createTable('Ghost_Users', (t) => {
-        
+        t.increments('ID');
+        t.integer('Points').notNullable();
+        t.enu('Winner', [0, 1, 2], {
+          useNative: true,
+          enumName: 'winner',
+        });
+        t.enu('Type', ['WRITING', 'DRAWING'], {
+          useNative: true,
+          enumName: 'subtype',
+        });
+        t.integer('SquadID')
+          .notNullable()
+          .unsigned()
+          .references('Squads.ID')
+          .onUpdate('CASCADE')
+          .onDelete('RESTRICT');
+      })
     });
 };
 
 exports.down = function (knex) {
     return knex.schema
         .dropTableIfExists('Ghost_Users')
+        .raw('DROP TYPE winner')
+        .raw('DROP TYPE subtype');
 };
