0a6adbf4e5a0269799049c827c29f59bfa1f1753
christopher harwell <chrisharwell72@gmail.com>
2020-11-11 14:25:54 -0500

    Creates Ghost_Users table schema

--- data/migrations/20201104190917_gamification-bots.js
+++ data/migrations/20201104190917_gamification-bots.js
@@ -3,30 +3,53 @@ exports.up = function (knex) {
   return knex.schema.createTable('Ghost_Users', (t) => {
     t.increments('ID');
     t.integer('Points').notNullable();
-    t.enu('Winner', [0, 1, 2], {
-      useNative: true,
-      enumName: 'winner',
-    });
-    t.enu('Type', ['WRITING', 'DRAWING'], {
-      useNative: true,
-      enumName: 'subtype',
-    });
-    t.integer('SubmissionID1')
-      .notNullable()
-      .unsigned()
-      .references('Submissions.ID')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT');
-    t.integer('SubmissionID2')
-      .notNullable()
-      .unsigned()
-      .references('Submissions.ID')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT');
     t.integer('SquadID')
       .notNullable()
       .unsigned()
       .references('Squads.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+  });
+  .createTable('Bot_Submissions', (t) => {
+    t.increments('ID');
+    t.integer('ChildID')
+      .notNullable()
+      .unsigned()
+      .references('Children.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+    t.integer('Bot_StoryID')
+      .notNullable()
+      .unsigned()
+      .references('Bot_Stories.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+    t.boolean('HasRead').defaultTo(false);
+    t.boolean('HasWritten').defaultTo(false);
+    t.boolean('HasDrawn').defaultTo(false);
+    t.integer('Complexity');
+    t.unique(['ChildID', 'StoryID']);
+  })
+  .createTable('Bot_Writing', (t) => {
+    t.increments('ID');
+    t.string('URL').notNullable().unique();
+    t.integer('PageNum').notNullable();
+    t.integer('Bot_SubmissionID')
+      .notNullable()
+      .unsigned()
+      .references('Bot_Submissions.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+    t.unique(['PageNum', 'Bot_SubmissionID']);
+  })
+  .createTable('Bot_Drawing', (t) => {
+    t.increments('ID');
+    t.string('URL').notNullable().unique();
+    t.integer('Bot_SubmissionID')
+      .notNullable()
+      .unsigned()
+      .unique()
+      .references('Bot_Submissions.ID')
       .onUpdate('CASCADE')
       .onDelete('RESTRICT');
   });
@@ -35,6 +58,7 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema
     .dropTableIfExists('Ghost_Users')
-    .raw('DROP TYPE winner')
-    .raw('DROP TYPE subtype');
+    .dropTableIfExists('Bot_Drawing')
+    .dropTableIfExists('Bot_Writing')
+    .dropTableIfExists('Bot_Submissions');
 };
