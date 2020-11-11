be1a0d59e18e83b21be755b1027f27a3ca6aebe3
christopher harwell <chrisharwell72@gmail.com>
2020-11-11 02:03:20 -0500

    adds submission id to the Ghost Users migration

--- data/migrations/20201104190917_gamification-bots.js
+++ data/migrations/20201104190917_gamification-bots.js
@@ -1,29 +1,40 @@
 
 exports.up = function (knex) {
-    return knex.schema.createTable('Ghost_Users', (t) => {
-        t.increments('ID');
-        t.integer('Points').notNullable();
-        t.enu('Winner', [0, 1, 2], {
-          useNative: true,
-          enumName: 'winner',
-        });
-        t.enu('Type', ['WRITING', 'DRAWING'], {
-          useNative: true,
-          enumName: 'subtype',
-        });
-        t.integer('SquadID')
-          .notNullable()
-          .unsigned()
-          .references('Squads.ID')
-          .onUpdate('CASCADE')
-          .onDelete('RESTRICT');
-      })
+  return knex.schema.createTable('Ghost_Users', (t) => {
+    t.increments('ID');
+    t.integer('Points').notNullable();
+    t.enu('Winner', [0, 1, 2], {
+      useNative: true,
+      enumName: 'winner',
     });
+    t.enu('Type', ['WRITING', 'DRAWING'], {
+      useNative: true,
+      enumName: 'subtype',
+    });
+    t.integer('SubmissionID1')
+      .notNullable()
+      .unsigned()
+      .references('Submissions.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+    t.integer('SubmissionID2')
+      .notNullable()
+      .unsigned()
+      .references('Submissions.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+    t.integer('SquadID')
+      .notNullable()
+      .unsigned()
+      .references('Squads.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+  });
 };
 
 exports.down = function (knex) {
-    return knex.schema
-        .dropTableIfExists('Ghost_Users')
-        .raw('DROP TYPE winner')
-        .raw('DROP TYPE subtype');
+  return knex.schema
+    .dropTableIfExists('Ghost_Users')
+    .raw('DROP TYPE winner')
+    .raw('DROP TYPE subtype');
 };
