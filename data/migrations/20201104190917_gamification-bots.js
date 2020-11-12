255cc6e98c5c67d5adda6bc7563bad57a9dc0d4d
christopher harwell <chrisharwell72@gmail.com>
2020-11-12 11:13:54 -0500

    removes foreign key constraint from Bot migratian

--- data/migrations/20201104190917_gamification-bots.js
+++ data/migrations/20201104190917_gamification-bots.js
@@ -2,56 +2,28 @@
 exports.up = function (knex) {
   return knex.schema.createTable('Ghost_Users', (t) => {
     t.increments('ID');
-    t.integer('Points').notNullable();
-    t.integer('SquadID')
-      .notNullable()
-      .unsigned()
-      .references('Squads.ID')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT');
+    t.integer('Points').nullable();
+    t.integer('SquadID').notNullable()
   })
   .createTable('Bot_Submissions', (t) => {
     t.increments('ID');
-    t.integer('ChildID')
-      .notNullable()
-      .unsigned()
-      .references('Children.ID')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT');
-    t.integer('Bot_StoryID')
-      .notNullable()
-      .unsigned()
-      .references('Stories.ID')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT');
+    t.integer('Bot_StoryID').notNullable()
     t.boolean('HasRead').defaultTo(false);
     t.boolean('HasWritten').defaultTo(false);
     t.boolean('HasDrawn').defaultTo(false);
     t.integer('Complexity');
-    t.unique(['ChildID', 'Bot_StoryID']);
   })
   .createTable('Bot_Writing', (t) => {
     t.increments('ID');
-    t.string('URL').notNullable().unique();
-    t.integer('PageNum').notNullable();
-    t.integer('Bot_SubmissionID')
-      .notNullable()
-      .unsigned()
-      .references('Bot_Submissions.ID')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT');
+    t.string('URL').nullable().unique();
+    t.integer('PageNum').nullable();
+    t.integer('Bot_SubmissionID').notNullable()
     t.unique(['PageNum', 'Bot_SubmissionID']);
   })
   .createTable('Bot_Drawing', (t) => {
     t.increments('ID');
-    t.string('URL').notNullable().unique();
-    t.integer('Bot_SubmissionID')
-      .notNullable()
-      .unsigned()
-      .unique()
-      .references('Bot_Submissions.ID')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT');
+    t.string('URL').nullable().unique();
+    t.integer('Bot_SubmissionID').unique()
   });
 };
 
