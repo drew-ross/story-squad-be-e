d608e2afc24c5d1f74ecf56a2a733e64b2e86186
christopher harwell <chrisharwell72@gmail.com>
2020-11-12 11:14:28 -0500

    Adds brackets to where there supposed to be

--- data/seeds/007_GhostUsers.js
+++ data/seeds/007_GhostUsers.js
@@ -1,12 +1,12 @@
 const faker = require('faker');
 
-const Ghost_Users = [...new Array(3).map(() => ({
-  points: `${faker.random.number({ min: 0, max: 25 })}`,
+const Ghost_Users = [...new Array(3)].map((i, idx) => ({
+  Points: `${faker.random.number({ min: 0, max: 25 })}`,
   // idx may need to be changed later to better reflect actual Squad IDs
   SquadID: idx, 
-}))];
+}));
 
 exports.seed = function(knex) {
-      knex('Ghost_Users').insert(Ghost_Users);
+  return knex('Ghost_Users').insert(Ghost_Users);
       
 };
