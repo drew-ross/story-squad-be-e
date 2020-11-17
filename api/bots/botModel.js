dc533eaa64683cdd7187adac8f9a8686d924f722
christopher harwell <chrisharwell72@gmail.com>
2020-11-17 15:49:28 -0500

    removes botModel file because it was dead code

--- api/bots/botModel.js
+++
@@ -1,5 +0,0 @@
-// const db = require('../../data/db-config');
-const router = require('express').Router();
-const Game = require('../game/gameModel');
-const { authRequired } = require('../middleware');
-
